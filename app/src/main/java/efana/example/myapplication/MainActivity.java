dbecff64d0547d3d39598c1792fb7ff3895b7266
Efana Usua <usuaefana@gmail.com>
2019-08-22 09:52:15 +0100

    created recycler view and finished up the adapter ,hooked up retrofit, picasso and the recyclerview

--- app/src/main/java/efana/example/myapplication/MainActivity.java
+++ app/src/main/java/efana/example/myapplication/MainActivity.java
@@ -1,13 +1,69 @@
 package efana.example.myapplication;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
+import java.util.List;
+
+import efana.example.myapplication.adapter.CustomAdapter;
+import efana.example.myapplication.model.RetroPhoto;
+import efana.example.myapplication.network.GetDataService;
+import efana.example.myapplication.network.RetrofitClientInstance;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+/*
+ Tutorial link for reference https://medium.com/@prakash_pun/retrofit-a-simple-android-tutorial-48437e4e5a23
+
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private CustomAdapter adapter;
+    private RecyclerView recyclerView;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        /*Create handle for the RetrofitInstance interface*/
+        GetDataService service = RetrofitClientInstance.getRetrofit().create(GetDataService.class);
+        Call<List<RetroPhoto>> call = service.getAllPhotos();
+        call.enqueue(new Callback<List<RetroPhoto>>() {
+            @Override
+            public void onResponse(Call<List<RetroPhoto>> call, Response<List<RetroPhoto>> response) {
+                progressDialog.dismiss();
+                generateDataList(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<RetroPhoto>> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Something went wrong... Please try again later!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    /*Method to generate List of data using RecyclerView with custom adapter*/
+    private void generateDataList(List<RetroPhoto> photoList) {
+        recyclerView = findViewById(R.id.customRecyclerView);
+        adapter = new CustomAdapter(this, photoList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+
     }
 }
--- app/src/main/java/efana/example/myapplication/adapter/CustomAdapter.java
+++ app/src/main/java/efana/example/myapplication/adapter/CustomAdapter.java
@@ -2,11 +2,18 @@ package efana.example.myapplication.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import efana.example.myapplication.R;
 import efana.example.myapplication.model.RetroPhoto;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
@@ -20,24 +27,45 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
     }
 
     public class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        public final View mView;
+
+        TextView txtTitle;
+        private ImageView coverImage;
+
         public CustomViewHolder(View itemView) {
             super(itemView);
+            mView = itemView;
+
+            txtTitle = mView.findViewById(R.id.title);
+            coverImage = mView.findViewById(R.id.cover_image);
         }
     }
 
     @Override
-    public CustomViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        return null;
+    public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.custom_row, parent, false);
+
+        return new CustomViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder (CustomViewHolder customViewHolder, int i) {
+    public void onBindViewHolder (CustomViewHolder holder, int position) {
+        holder.txtTitle.setText(dataList.get(position).getTitle());
+
+        Picasso.Builder builder = new Picasso.Builder(context);
+        builder.downloader(new OkHttp3Downloader(context));
+        builder.build().load(dataList.get(position).getThumbnailUrl())
+                .placeholder((R.drawable.ic_launcher_background))
+                .error(R.drawable.ic_launcher_background)
+                .into(holder.coverImage);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dataList.size();
     }
 
 
